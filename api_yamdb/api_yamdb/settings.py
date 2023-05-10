8728c730adf032657eabd4af2fbcc20c42210e7c
Иван Пономарев <panama.piv@yandex.ru>
2023-05-10 22:09:43 +0200

    deploy

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -9,7 +9,7 @@ SECRET_KEY = os.getenv('SECRET_KEY',
                        'p&l%385148kslhtyn^##a1)ilz@4zqj=rq&agdol^##zgl9(vs')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv('DEBUG', 'True') in 'True'
 
 ALLOWED_HOSTS = [os.getenv('PROD_HOST', 'web')]
 
